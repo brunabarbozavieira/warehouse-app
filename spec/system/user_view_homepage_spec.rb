1c9729541582c906c5be3dd4dec8990500d27d75
Bruna Barboza Vieira <bruna.barboza111@gmail.com>
2022-09-18 17:28:14 -0300

    Usuário vê galpões na tela inicial

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -1,8 +1,31 @@
 require 'rails_helper'
 
 describe 'Usuário visita a tela inicial' do 
-    it 'e vê o nome da app' do 
-        visit('/')
-        expect(page).to have_content('Galpões & Estoque')
-    end
+  it 'e vê o nome da app' do 
+    visit('/')
+    expect(page).to have_content('Galpões & Estoque')
+  end
+
+  it 'e vê os galpões cadastrados' do 
+    Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000)
+    Warehouse.create(name: 'Maceio', code: 'MCZ', city: 'Maceio', area: 50_000)
+
+    visit('/')
+
+    expect(page).not_to have_content('Não existem galpões cadastrados')
+    expect(page).to have_content('Rio')
+    expect(page).to have_content('Código: SDU')
+    expect(page).to have_content('Cidade: Rio de Janeiro')
+    expect(page).to have_content('60000 m²')
+
+    expect(page).to have_content('Maceio')
+    expect(page).to have_content('Código: MCZ')
+    expect(page).to have_content('Cidade: Maceio')
+    expect(page).to have_content('50000 m²')
+  end
+  it 'e não existem galpões cadastrados' do 
+    visit('/')
+
+    expect(page).to have_content('Não existem galpões cadastrados')
+  end 
 end
